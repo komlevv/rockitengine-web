ccf9a626c0b840fa7c9607560f3616b6b2175d2c
komlevv <komlevv@users.noreply.github.com>
2022-08-04 14:28:07 +0300

    Fix html unused import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App.js';
-import html from './index.html'
+import './index.html'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
