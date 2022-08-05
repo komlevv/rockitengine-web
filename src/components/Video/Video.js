db727e2d474e7a84e6339e8f908d31d11d90d1cf
komlevv <komlevv@users.noreply.github.com>
2022-08-05 10:28:22 +0300

    Fix React does not set muted attribute

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,21 +1,28 @@
 import s from "./Video.css"
+import {createRef, useEffect} from "react";
 
 const Video = ({
-                   src, poster, loop, muted,
-                   controls, autoplay, innerRef
-               }) => (
+                 src, poster, loop, muted,
+                 controls, autoplay, innerRef = createRef()
+               }) => {
+  useEffect(() => {
+    // tbd, see https://github.com/facebook/react/issues/10389
+    muted && innerRef.current.setAttribute('muted', '')
+  })
+  return (
     <video
-        ref={innerRef}
-        className={s.video}
-        controls={controls}
-        poster={poster}
-        loop={loop}
-        muted={muted}
-        autoPlay={autoplay}
+      ref={innerRef}
+      className={s.video}
+      controls={controls}
+      poster={poster}
+      loop={loop}
+      muted={muted}
+      autoPlay={autoplay}
     >
-        <source src={src} type="video/mp4"/>
-        Your browser does not support the video tag.
+      <source src={src} type="video/mp4"/>
+      Your browser does not support the video tag.
     </video>
-)
+  )
+}
 
 export default Video;
