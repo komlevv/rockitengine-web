368e4e649963a84008cb6c76d6e0635f507eec49
komlevv <komlevv@users.noreply.github.com>
2022-08-05 10:57:05 +0300

    Add favicon

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App.js';
 import './index.html'
-
+import './favicon.ico'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App/>);
