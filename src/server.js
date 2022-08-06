3b520c69d136caa137cde983ba5c93f1ed95e01f
komlevv <komlevv@users.noreply.github.com>
2022-08-06 11:56:11 +0300

    Basic Docker setup

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import webpackMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import config from '../webpack.config';
 
-const host = '192.168.56.40';
+const host = '0.0.0.0'; // listen on all interfaces
 const port = 8080;
 const app = express();
 
