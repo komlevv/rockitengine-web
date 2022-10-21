8f1a6a57080d1fbc863b1ba1d3abfceb088628ea
komlevv <komlevv@users.noreply.github.com>
2022-10-21 09:44:46 +0300

    Add custom video player

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -4,6 +4,27 @@ import Image from '../Image/Image';
 import Spinner from '../Spinner/Spinner';
 import { useIntersectionObserver } from '../../hooks/useIntersectionObserver';
 
+// todo: this is a Work in Progress
+const IconPlay = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14.3059 16.5191">
+    <polygon fill="currentColor" points="14.306 8.26 0 0 0 16.519 14.306 8.26" />
+  </svg>
+);
+const IconPause = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 14">
+    <g fill="currentColor">
+      <rect width="3" height="14" />
+      <rect x="7" width="3" height="14" />
+    </g>
+  </svg>
+);
+
+const PlayButton = ({ onClick, text, cls, ...props }) => (
+  <button type="button" onClick={onClick} className={`${s.button} ${cls}`} {...props}>
+    {text}
+  </button>
+);
+
 const Video = ({
   src,
   poster,
@@ -42,10 +63,62 @@ const Video = ({
 
   useEffect(() => {
     if (muted && videoShow) videoRef.current.setAttribute('muted', '');
-  }, [videoRef, videoShow]);
+  }, [videoShow]);
+
+  const [buttonIcon, setButtonIcon] = useState(IconPlay);
+  const [progressHidden, setProgressHidden] = useState(true);
+  const [playing, setPlaying] = useState(false);
+  const [cursorClass, setCursorClass] = useState('');
+  const [buttonHidden, setButtonHidden] = useState('');
+
+  const handleButtonClick = () => {
+    if (!buttonHidden) setButtonHidden(s.hide);
+    if (!cursorClass) setCursorClass(s.cursorDefault);
+    if (progressHidden) setProgressHidden(false);
+    if (!playing) {
+      videoRef.current.play();
+      setPlaying(true);
+      setButtonIcon(IconPause);
+    } else {
+      videoRef.current.pause();
+      setPlaying(false);
+      setButtonIcon(IconPlay);
+      setButtonHidden('');
+    }
+  };
+
+  const handleVideoEnter = () => {
+    if (videoShow) {
+      if (playing && buttonHidden) {
+        setButtonHidden('');
+        setProgressHidden(false);
+      }
+    }
+  };
+
+  const handleVideoLeave = () => {
+    if (videoShow) {
+      if (playing) setProgressHidden(true);
+      if (playing && !buttonHidden) {
+        setButtonHidden(s.hide);
+      }
+    }
+  };
+
+  const [progressValue, setProgressValue] = useState(10);
+  const [progressMax, setProgressMax] = useState(0);
+  const handleTimeUpdate = () => {
+    if (progressMax === 0) setProgressMax(videoRef.current.duration);
+    setProgressValue(videoRef.current.currentTime);
+  };
 
   return (
-    <div ref={animationRef} className={containerCls || s.videoContainer}>
+    <div
+      onMouseEnter={handleVideoEnter}
+      onMouseLeave={handleVideoLeave}
+      ref={animationRef}
+      className={containerCls || s.videoContainer}
+    >
       <Spinner style={thumbnailHidden ? { display: 'none' } : {}} />
       <Image
         spinner={false}
@@ -54,21 +127,40 @@ const Video = ({
         containerStyle={thumbnailHidden ? { visibility: 'hidden' } : {}}
       />
       {videoShow && (
-        <video
-          ref={videoRef}
-          className={s.video}
-          // todo: setting poster here makes poster image load twice on clients with disabled cache
-          poster={poster}
-          preload="metadata"
-          onLoadedMetadata={handleLoadedMetadata}
-          controls={controls}
-          loop={loop}
-          muted={muted}
-          autoPlay={autoplay}
-        >
-          <source src={src} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+        <>
+          {controls && (
+            <>
+              <progress
+                className={s.progress}
+                value={progressValue}
+                max={progressMax}
+                style={progressHidden ? { visibility: 'hidden' } : {}}
+              />
+              <PlayButton
+                style={!thumbnailHidden ? { visibility: 'hidden' } : {}}
+                onClick={handleButtonClick}
+                text={buttonIcon}
+                cls={buttonHidden}
+              />
+            </>
+          )}
+
+          <video
+            ref={videoRef}
+            className={`${s.video} ${cursorClass}`}
+            poster={poster}
+            preload="metadata"
+            onClick={handleButtonClick}
+            onLoadedMetadata={handleLoadedMetadata}
+            onTimeUpdate={handleTimeUpdate}
+            loop={loop}
+            muted={muted}
+            autoPlay={autoplay}
+          >
+            <source src={src} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </>
       )}
     </div>
   );
