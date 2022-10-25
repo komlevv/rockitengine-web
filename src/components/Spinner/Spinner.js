0aa74b4377e05bab0c3ccfda460a388379544d6e
komlevv <komlevv@users.noreply.github.com>
2022-10-25 11:21:47 +0300

    Spinner fixes
    - make spinner thinner
    - disable pointer events

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -3,7 +3,7 @@ import s from './Spinner.scss';
 const Spinner = ({ style }) => (
   <div className={s.container} style={style}>
     <svg className={s.spinner} viewBox="0 0 50 50">
-      <circle className={s.path} cx="25" cy="25" r="20" fill="none" strokeWidth="5" />
+      <circle className={s.path} cx="25" cy="25" r="20" fill="none" strokeWidth="4" />
     </svg>
   </div>
 );
