83ad5e2fdf55be7c079bf272afe1e23f3a456333
komlevv <komlevv@users.noreply.github.com>
2022-10-26 11:59:34 +0300

    Modify Gap component to take 'size' parameter

--- src/components/Gap/Gap.js
+++ src/components/Gap/Gap.js
@@ -1,11 +1,9 @@
-import s from './Gap.scss';
-
 // this is a hack for edge cases where animated group of components
 // does not have a parent container and is mapped directly into content[].
 // intended gap is 30px, so 10px padding from local css + 20px css grid gap
 // using this to speed up release, but ideally a separate component should
 // be generated for such cases, and GridContainer should set padding-bottom
 // for direct children
-const Gap = () => <div className={s.gap} />;
+const Gap = ({ size = 10 }) => <div style={{ paddingBottom: `${size}px` }} />;
 
 export default Gap;
